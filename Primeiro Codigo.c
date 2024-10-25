2fde58a92593a7086477646b790d5c4584e3d3cc
yAkira028 <117579923+yAkira028@users.noreply.github.com>
2024-10-25 20:10:41 -0300

    Opção de saída.
    Incluso no sistema a opção de saída.

--- Primeiro Codigo.c
+++ Primeiro Codigo.c
@@ -122,6 +122,7 @@ int main(){
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema \n\n");
 		printf("Opção: "); //fim do menu
 		
 		scanf("%d", &opcao); //armazenando a escolha do usuario
@@ -138,6 +139,10 @@ int main(){
 			break;
 			case 3:
 			deletar();
+			break;
+			case 4:
+			printf("Obrigado por utilizar o sistema! \n");
+			return 0; 
 			break;
 			
 			default:
